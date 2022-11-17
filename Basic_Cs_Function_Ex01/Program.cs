13aab3d0051c1e5f71da85ccbdb21acac709df84
PC <PC@DESKTOP-8PSQBN9>
2022-11-17 22:42:51 +0700

    Add project files.

---
+++ Basic_Cs_Function_Ex01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_Cs_Function_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
